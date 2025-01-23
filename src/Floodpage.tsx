918e5c27f9e2ed9d1e88b1a69dcd1bb8e76a5a7f
Yugalbansal <bansalyugal3@gmail.com>
2025-01-23 09:30:29 +0530

    add flood page

---
+++ src/Floodpage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const FloodsPage: React.FC = () => {
+  return (
+    <div>
+      <h1>Floods</h1>
+      <p>Real-time flood risk assessment details...</p>
+    </div>
+  );
+};
+
+export default FloodsPage;
